9c0788bdcc5b22f5ff3eb369b9b221cbbb698ee2
Jeong-heon2 <cjhgg2@gmail.com>
2020-05-05 23:07:19 +0900

    순열 알고리즘

--- src/algorithm/Permutation.java
+++ src/algorithm/Permutation.java
@@ -4,13 +4,12 @@ public class Permutation {
 
 	public static void main(String[] args) {
 		int[] arr = {1,2,3};
-		perm(arr,0,3,1);
+		perm(arr,0,3);
 		
 	}
-	//depth는 교환 트리구조에서 깊이를 의미,  n은 배열의 크기, k는 몇개를 뽑아서 순열을 만들것인가?
-	//depth가 k와 같아지면 더이상 순열을 만들어내지않는다.
-	public static void perm(int[] arr, int depth, int n,int k) {
-		if(depth == k) {
+	//depth는 교환 트리구조에서 깊이 를 의미. n은 배열의 크기
+	public static void perm(int[] arr, int depth, int n) {
+		if(depth == n) {
 			for(int i = 0 ; i < n ; i ++) {
 				System.out.print(arr[i] + " ");
 			}
@@ -19,10 +18,11 @@ public class Permutation {
 		}
 		for(int i = depth ; i < n ; i ++) {
 			swap(arr, i , depth);
-			perm(arr, depth+1, n,k);
+			perm(arr, depth+1, n);
 			swap(arr, i , depth);
 		}
 	}
+	//교환하는 함수
 	public static void swap(int[]arr , int i , int j) {
 		int temp = arr[i];
 		arr[i] = arr[j];
