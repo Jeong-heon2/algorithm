b1b979d58a5ce854bd578ea43c474fb2b6f5a364
Jeong-heon2 <cjhgg2@gmail.com>
2020-09-05 19:12:56 +0900

    kakao 2018 blind 뉴스 클러스터링 hashmap

--- src/kakao/Kakao2018BlindQ2.java
+++ src/kakao/Kakao2018BlindQ2.java
@@ -1,110 +1,56 @@
 package kakao;
-
-import utils.ArrayInputConvert;
-
+import java.util.*;
+import java.util.Map.Entry;
 public class Kakao2018BlindQ2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println(ArrayInputConvert.convertInput("[[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,4,0,0,0],[0,0,0,0,0,4,4,0,0,0],[0,0,0,0,3,0,4,0,0,0],[0,0,0,2,3,0,0,0,5,5],[1,2,2,2,3,3,0,0,0,5],[1,1,1,0,0,0,0,0,0,5]]"));
-		int[][] board = {{0,0,0,0,0,0,0,0,0,7},{0,0,0,0,0,0,0,7,7,7},{0,0,0,0,0,0,0,0,0,0},{0,0,0,0,0,0,0,0,0,0},{0,0,0,0,0,0,4,0,0,0},{0,0,0,0,0,4,4,0,0,0},{0,0,0,0,3,0,4,0,0,0},{0,0,0,2,3,0,0,0,5,5},{1,2,2,2,3,3,0,0,0,5},{1,1,1,0,0,0,0,0,0,5}};
-		Solution sl = new Solution();
-		System.out.println(sl.solution(board));
 
 	}
-	static class Solution {
-	    int height;
-	    int N;
-	    int ans;
-	    public int solution(int[][] board) {
-	        ans = 0;
-	        //1.가장 높은 블록의 y 위치 부터 검은 블록을 다 체운다. 
-	        //2. 가장 높은 블록의 y, 0 부터 2*3  3*2 직사각형을 탐색해서
-	        //3. 검은 블록 2개 색깔블록 4개가 직사각형에 있으면 삭제 하고 개수 카운팅후 1번 부터 반복
-	        N = board.length;
-	        height = 0;
-	        boolean find = false;
-	        for(int i = 0 ; i < N ; i++){
-	            for(int j = 0 ; j < N ; j++){
-	            	if(board[i][j] != 0){
-	                    height = i;
-	                    find = true;
-	                    break;
-	                }
-	            }
-	            if(find) break;
+	class Solution {
+	    public int solution(String str1, String str2) {
+	        int equals = 0;
+	        int sum = 0;
+	        HashMap<String, Integer> set1 = getSet(str1);
+	        HashMap<String, Integer> set2 = getSet(str2);
+	        if(set1.size() == 0 && set2.size()==0){
+	            return 65536;
 	        }
-	        setBlack(board);
-	        for(int i = height; i < N ; i++){
-	            for(int j = 0 ; j < N ; j++){
-	                if(i+2 < N && j+1 < N){
-	                    if(detactAndDelete(board, 3+i, 2+j, i, j)){
-	                        setBlack(board);
-	                        i = height - 1;
-	                        break;
-	                    }
-	                }
-	                if(i+1 < N && j + 2 < N){
-	                    if(detactAndDelete(board, 2+i, 3+j, i, j)){
-	                        setBlack(board);
-	                        i = height - 1;
-	                        break;
-	                    }
-	                }
+	        for(Entry<String, Integer> e : set1.entrySet()) {
+				String key = e.getKey();
+	            int v = e.getValue();
+	            if(set2.containsKey(key)){
+	                int v2 = set2.get(key);
+	                equals += Math.min(v, v2);
+	                sum += Math.max(v, v2);
+	            }else{
+	                sum += v;
 	            }
-	        }
+			}
+	        for(Entry<String, Integer> e : set2.entrySet()) {
+				String key = e.getKey();
+	            int v = e.getValue();
+	            if(!set1.containsKey(key)) sum += v;
+			}
+	        double res = (double)equals/(double)sum;
 	        
-	        
-	        return ans;
+	        return (int)(res*65536);
 	    }
-	    public void setBlack(int[][] arr){
-	        //height ~ N-1 까지 검은색 블록을 체운다 (조건에 맞게)
-	        boolean[] visited = new boolean[N];
-	        for(int i = height ; i < N ; i++){
-	            for(int j = 0 ; j < N ; j++){
-	                if(arr[i][j] != 0 && arr[i][j] != -1){
-	                    visited[j] = true;
-	                    continue;
-	                }
-	                if(!visited[j]){
-	                    arr[i][j] = -1;
-	                }
-	            }
-	        }
-	    }
-	    public boolean detactAndDelete (int[][] arr, int len_y, int len_x, int y, int x){
-	        int black_cnt = 0;
-	        int color = -1;
-	        int color_cnt = 0;
-	        for(int i = y ; i < len_y ; i++){
-	            for(int j = x ; j < len_x ; j++){
-	                if(arr[i][j] == 0) break;
-	                else if(arr[i][j] == -1) black_cnt++;
-	                else{
-	                    if(color == -1){
-	                        color = arr[i][j];
-	                        color_cnt++;
-	                    }else{
-	                        if(color != arr[i][j]) break;
-	                        else color_cnt++;
-	                    }
+	    //문자열 2개씩 끊어서 집합 만들기
+	    public HashMap<String, Integer> getSet(String str){
+	        HashMap<String, Integer> map = new HashMap<>();
+	        for(int i = 0 ; i < str.length()-1 ; i++){
+	            String s = str.substring(i, i+2).toLowerCase();
+	            if(s.matches("^[a-zA-X]+$")){
+	                if(map.containsKey(s)){
+	                    int cnt = map.get(s);
+	                    map.put(s, ++cnt);
+	                }else{
+	                    map.put(s, 1);
 	                }
 	            }
 	        }
-	        if(black_cnt == 2 && color_cnt == 4) {
-	            delete(arr, len_y, len_x, y, x);
-	            ans++;
-	            return true;
-	        }else{
-	            return false;
-	        }
-	    }
-	    public void delete(int[][] arr, int len_y, int len_x, int y, int x){
-	        for(int i = y ; i < len_y ; i++){
-	            for(int j = x; j < len_x; j++){
-	                arr[i][j] = 0;
-	            }
-	        }
+	        return map;
 	    }
 	}
 
