2cc398bf47df1736f589eafc38a41f17c99fd835
Jeong-heon2 <cjhgg2@gmail.com>
2021-01-26 19:20:35 +0900

    백준 10655 dp

--- src/dynamic/Q10655.java
+++ src/dynamic/Q10655.java
@@ -4,13 +4,39 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
 public class Q10655 {
-
-	public static void main(String[] args) throws Exception {
-		// TODO Auto-generated method stub
-		//        i-1까지에서 한개 건너뛴 거리의 최소 + i노드와 이전노드거리   ,   i-2까지 쭉 달려온 거리 +  i-1 건너뛰고 i-2와 i 이은 거
-		//dp[i] = dp[i-1] + distance(i, 이전노드 )  , arr[i-2] + distance(i-2, i)
+	
+	public static void main(String[] args) throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
+		//dp[i] = min( dp[i-1] + distance(i, i-1) , arr[i-2] + distance(i, i-2) ) 
+		int N = Integer.parseInt(br.readLine());
+		int[] dp = new int[N];
+		int[] arr = new int[N];
+		Node[] nodes = new Node[N];
+		for(int i = 0 ; i < N ; i++) {
+			String[] tmp = br.readLine().split(" ");
+			nodes[i] = new Node(Integer.parseInt(tmp[0]), Integer.parseInt(tmp[1]));
+		}
+		arr[0] = 0;
+		arr[1] = getDistance(nodes[0], nodes[1]);
+		dp[0] = 0;
+		dp[1] = getDistance(nodes[0], nodes[1]);
+		int pre = 0;
+		for(int i = 2 ; i < N ; i++) {
+			int tmp = getDistance(nodes[i], nodes[i-1]);
+			arr[i] = arr[i-1] + tmp;
+			dp[i] = Math.min(dp[i-1] + tmp , arr[i-2] + getDistance(nodes[i], nodes[i-2]));
+		}
+		System.out.println(dp[N-1]);
 	}
-
+	public static int getDistance(Node i, Node j) {
+		return Math.abs(i.x - j.x) + Math.abs(i.y - j.y);
+	}
+	static class Node{
+		int x;
+		int y;
+		Node(int x, int y){
+			this.x = x;
+			this.y = y;
+		}
+	}
 }
